637cf5722a8a9d155f0ecead425022929917c000
kaushik94 <kaushik.varanasi1@gmail.com>
2015-05-14 00:36:32 +0530

    init

---
+++ opener.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Firefox()
+
+def _cp_open(url="localhost:4545/paper/new"):
+	driver.get(url)
+
+def _cp_close():
+	driver.close()
+
+def _credentials(uname="fizz", email_id="buzz@beer.com"):
+	c_name = driver.find_element_by_id('candidateName')
+	c_name.send_keys(uname)
+	email = driver.find_element_by_id('candidateEmail')
+	email.send_keys(email_id)
+	button = driver.find_element_by_xpath("//*[contains(text(), 'Join as candidate')]")
+	button.send_keys(Keys.RETURN)
+
+_cp_open()
+_credentials()
+# _cp_close()
+
+'''
+candidateName
+candidateEmail
+interviewerName
+accessCode
+'''
